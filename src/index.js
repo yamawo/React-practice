2637f3fb2a71ac3c36c21dc353ef037a9b7210d2
yamawo <y7a3m1asaki@gmail.com>
2019-11-07 18:52:53 +0900

    use hook

--- src/index.js
+++ src/index.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
-
-const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
     }
-    
-    return (
+
+    return(
         <div>
-            <Header course={} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            button press history: {props.allClicks.join(' ')}
         </div>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+const App = (props) => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    return (
+        <div>
+            <div>
+                {left}
+                <Button onClick={handleLeftClick} text='left' />
+                <Button onClick={handleRightClick} text='right' />
+                {right}
+                <History allClicks={allClicks} />
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
